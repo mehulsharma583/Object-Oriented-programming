54f3d9cd77fd146c4b9a534d4ee751beb076904c
mehulsharma583 <mehul.sharma@student.adelaide.edu.au>
2025-02-25 18:06:18 +1030

    Update function-1-3.cpp

---
+++ function-1-3.cpp
@@ -0,0 +1,6 @@
+//1-3. Write a function that returns the number of elements in an array that are equal to a given parameter. 
+//The function should take as parameters the array, its size, and the desired number.
+//The function should return 0 if the size parameter, n, is less than 1.
+
+Signature: int num_count(int array[], int n, int number)
+
